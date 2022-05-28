38c01e153d3e6ef0fadb6aae5abb8e765bf8de77
pantoniou <pantoniou@illumina.com>
2022-05-28 10:41:08 +0100

    changed model pk to  string because i noticed on the NAT database not all ids are uuids

--- tna_app/models.py
+++ tna_app/models.py
@@ -3,7 +3,9 @@ import uuid
 
 # Create your models here.
 class Record(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # Noticed some of the records on database have string id and not all are uuid
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.CharField(max_length=50, primary_key=True, editable=False)
     title = models.CharField(max_length=50, blank=True, null=True)
     scopeContent_description = models.CharField(max_length=1000, blank=True, null=True)
     citableReference = models.CharField(max_length=1000, blank=True, null=True)
--- tna_app/urls.py
+++ tna_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.get_id, name="home"),
-    path("records/<uuid:id>/", views.record_detail, name="record_detail"),
+    path("records/<id>/", views.record_detail, name="record_detail"),
 ]
